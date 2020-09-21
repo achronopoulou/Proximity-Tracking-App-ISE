91076372c176b14632d42efcf39531243b0d41e5
rishipandey125 <rpandey2@illinois.edu>
2020-09-21 13:26:04 -0500

    creating basic view - backend print functioanlity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //comment
     //create a feature that initiates bluetooth, looks for a device, connects, and reports the distance
-
+    //3 bags (same room (near), same home, and not in same building (far)
     /**
      *
      * @param savedInstanceState
@@ -16,5 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button connect_ble_button = (Button) findViewById(R.id.connect_ble);
+        final Button proximity_button = (Button) findViewById(R.id.proximity);
+
     }
 }
