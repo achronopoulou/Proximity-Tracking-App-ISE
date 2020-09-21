ffe91156faf0f95a925a2af01344f2acb0a8ae2f
rishipandey125 <rpandey2@illinois.edu>
2020-09-21 13:07:27 -0500

    added javadoc render

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //comment
     //create a feature that initiates bluetooth, looks for a device, connects, and reports the distance
+
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
