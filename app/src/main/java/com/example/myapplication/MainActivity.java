a272301ce59ff20fa284f3f656432d34d35a5c45
rishipandey125 <rpandey2@illinois.edu>
2020-09-21 14:31:23 -0500

    creating click buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,10 +12,6 @@ public class MainActivity extends AppCompatActivity {
     //comment
     //create a feature that initiates bluetooth, looks for a device, connects, and reports the distance
     //3 bags (same room (near), same home, and not in same building (far)
-    /**
-     *
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
         final Button connect_ble_button = (Button) findViewById(R.id.connect_ble);
         final Button proximity_button = (Button) findViewById(R.id.proximity);
         final TextView output = (TextView) findViewById(R.id.output);
-        output.setText("Hello");
+        output.setText("");
 
+        connect_ble_button.setOnClickListener(new View.OnClickListener() {
+            output.setText("Connecting to Device");
+        });
+
+        proximity_button.setOnClickListener(new View.OnClickListener() {
+            output.setText("Checking Proximity");
+        });
     }
 }
