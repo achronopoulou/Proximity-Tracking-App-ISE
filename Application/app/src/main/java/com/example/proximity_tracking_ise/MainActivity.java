af9f2e58d2f8a3c87961cfec562ae1ee0af18052
rishipandey125 <rpandey2@illinois.edu>
2020-10-11 19:44:00 -0500

    reuploaded and saved config folder

---
+++ Application/app/src/main/java/com/example/proximity_tracking_ise/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proximity_tracking_ise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
