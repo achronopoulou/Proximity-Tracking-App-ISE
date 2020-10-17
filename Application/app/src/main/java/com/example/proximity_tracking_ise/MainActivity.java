85015f79e47d26f7a1b00a527733658acd2fb72d
rishipandey125 <rpandey2@illinois.edu>
2020-10-17 18:58:18 -0500

    added bt enabled check

--- Application/app/src/main/java/com/example/proximity_tracking_ise/MainActivity.java
+++ Application/app/src/main/java/com/example/proximity_tracking_ise/MainActivity.java
@@ -2,16 +2,34 @@ package com.example.proximity_tracking_ise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Check to see if bluetooth is enabled
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 0);
+        } else {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, "Bluetooth Enabled", duration);
+            toast.show();
+        }
+
         setContentView(R.layout.activity_main);
     }
-    
 
     //connect two devices via BLE
     //One device is the GATT client (scans for advertisements, connects, requests data)
