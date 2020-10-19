feea97cd95077b0b4ea2a75ad29c6fe85569e9cb
rishipandey125 <rpandey2@illinois.edu>
2020-10-19 16:10:56 -0500

    implementing scanner

--- Application/app/src/main/java/com/example/proximity_tracking_ise/ProximityClient.java
+++ Application/app/src/main/java/com/example/proximity_tracking_ise/ProximityClient.java
@@ -1,21 +1,57 @@
 package com.example.proximity_tracking_ise;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 public class ProximityClient extends Service {
     @Override
+    private BluetoothLeScanner bleScanner;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onCreate() {
         //this is called when the service is first started (one-time)
+        //setup service things
+        // Device scan callback.
+        private ScanCallback leScanCallback =
+                new ScanCallback() {
+                    @Override
+                    public void onScanResult(int callbackType, ScanResult result) {
+                        super.onScanResult(callbackType, result);
+                        leDeviceListAdapter.addDevice(result.getDevice());
+                        leDeviceListAdapter.notifyDataSetChanged();
+                    }
+                };
+        bleScanner =  BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+
     }
 
     @Override
     public int onStartCommand() {
+        if (!scanning) {
+            bleScanner.startScan(leScanCallback);
+
+        }
+        //do everything start scan, get rssi, and stop scan
         return 0;
     }
 
     @Override
     public void onDestroy() {
         //this is called when the service is ending
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
 }
